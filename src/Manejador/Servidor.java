d329cbacf3815700782a2957a8dabf958d0c240b
Santuario Parra <lsantuariop1400@alumno.ipn.mx>
2018-03-29 13:24:29 -0600

    Act

--- src/Manejador/Servidor.java
+++ src/Manejador/Servidor.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Servidor {
+    
     public static void main(String []args) throws IOException {
         String comando="";
         ServerSocket servidor= new ServerSocket(3000);
