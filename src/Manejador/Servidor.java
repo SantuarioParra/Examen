a1dad3af826a7666875ef4ae9e662b303b256cb0
Santuario Parra <lsantuariop1400@alumno.ipn.mx>
2018-03-29 22:05:06 -0600

    Agregado metodo para crear tabla vacia, falta crear objetos dinamicos

--- src/Manejador/Servidor.java
+++ src/Manejador/Servidor.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class Servidor {
     public static void main(String []args) throws IOException {
@@ -39,7 +40,7 @@ public class Servidor {
 
                 //Inicio condicionales del servidor para las acciones de Create,Update,Delete,Insert,Show
 
-                if(comandoLimpio[0].compareTo("create")==0&&comandoLimpio[1].compareTo("database")==0){//if create table
+                if(comandoLimpio[0].compareTo("create")==0&&comandoLimpio[1].compareTo("database")==0){//if Creacion de tabla
 
                     if(basesDatos.containsKey(comandoLimpio[2])){
 
@@ -60,23 +61,41 @@ public class Servidor {
 
                     }
 
-                }else if(comandoLimpio[0].compareTo("set")==0&&comandoLimpio[1].compareTo("database")==0){
+                }else if(comandoLimpio[0].compareTo("set")==0&&comandoLimpio[1].compareTo("database")==0){// if poner(Set) base de datos
+
                     nombreSchema="";
                     nombreSchema=comandoLimpio[2];
                     outDatos.println("Base de datos: "+nombreSchema+" establecida");
                     outDatos.flush();
 
-                }else if(comandoLimpio[0].compareTo("create")==0&&comandoLimpio[1].compareTo("table")==0){
+                }else if(comandoLimpio[0].compareTo("create")==0&&comandoLimpio[1].compareTo("table")==0){//if Crear tabla en base de datos seleccionada
 
                     if(basesDatos.get(nombreSchema).containsKey(comandoLimpio[2])){
                         outDatos.println("Ya existe una tabla en la base de datos con nombre similar, seleccione un nuevo nombre.");
                         outDatos.flush();
                     }else {
+
                         System.out.println(comandoLimpio[2]);
                         basesDatos.get(nombreSchema).put(comandoLimpio[2], new LinkedList<>());
                         outDatos.println("Tabla " + comandoLimpio[2] + " creada en la base de datos " + nombreSchema);
                         System.out.println("Clave tabla: " + basesDatos.get(nombreSchema).get(comandoLimpio[2]));
                         outDatos.flush();
+                        //Creacion del objeto dinamico
+                        String[]datos= new String[comandoLimpio.length-6];
+                        Stack<String> parentesis= new Stack<>();
+                        for (int i=3; i<comandoLimpio.length;i++) {
+                            if(comandoLimpio[i].compareTo("(")==0){
+                                parentesis.push(comandoLimpio[i]);
+                            }else if(comandoLimpio[i].compareTo(")")==0){
+                                parentesis.pop();
+                            }else{
+                                datos[i]=comandoLimpio[i];
+                            }
+                        }
+                        for(int i=0;i<datos.length;i++){
+                            System.out.println(datos[i]);
+                        }
+                        //fin creaccion del objeto dinamico
 
 
                         for (HashMap.Entry entry : basesDatos.entrySet()) {
